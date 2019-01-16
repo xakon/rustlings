3fc849f3a75054987d12794fd16c5d99eff6db4f
Christos Kontas <xakon@yahoo.com>
2019-01-16 23:57:00 +0100

    iterators: solutions

--- standard_library_types/iterator3.rs
+++ standard_library_types/iterator3.rs
@@ -24,6 +24,17 @@ pub struct NotDivisibleError {
 // evenly divisible by b.
 // Otherwise, it should return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    }
+    else if a % b != 0 {
+        Err(DivisionError::NotDivisible(NotDivisibleError{
+            dividend: a, divisor: b,
+        }))
+    }
+    else {
+        Ok(a / b)
+    }
 }
 
 #[cfg(test)]
@@ -58,12 +69,11 @@ mod tests {
     }
 
     // Iterator exercises using your `divide` function
-    /*
     #[test]
     fn result_with_list() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x: Result<Vec<_>, DivisionError> = division_results.collect();
         assert_eq!(format!("{:?}", x), "Ok([1, 11, 1426, 3])");
     }
 
@@ -71,10 +81,9 @@ mod tests {
     fn list_of_results() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x: Vec<Result<_, DivisionError>> = division_results.collect();
         assert_eq!(format!("{:?}", x), "[Ok(1), Ok(11), Ok(1426), Ok(3)]");
     }
-    */
 }
 
 
